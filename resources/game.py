395349218f0edb23588a74a321cc90c9f02c916e
German Iakovlev <g.iakovlev@gmail.com>
2017-10-23 00:38:50 +0300

    Add Word API resource

--- resources/game.py
+++ resources/game.py
@@ -1,11 +1,12 @@
+import uuid
+
 from flask import jsonify, Blueprint, session
 from flask.ext.restful import (Resource, Api, reqparse,
                                inputs, fields, marshal,
                                marshal_with)
 
 from models_1 import Game, LetterGuessed
-import random
-import time
+
 from hangman import loadWords, chooseWord, getGuessedWord
 
 GAME_FIELDS = {
@@ -16,28 +17,20 @@ GAME_FIELDS = {
 
 
 class Word(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument(
-            'word',
-            required=True,
-            help='No word provided!',
-            location=['form', 'json']
-        )
-        super().__init__()
-
+    '''
+    Used once per game when the game is initialized.
+    '''
     def get(self):
+        '''
+        Generates random word, game UUID and creates a DB entry for a new game.
+        '''
         word = chooseWord(loadWords())
-        user_identifier = time.time()
-        repres = ''
-        for i in word:
-            repres += '_'
-        models_1.Game.create_game(user_identifier, word, len(word))
-        return jsonify(word_length=len(word), word=word, visual=repres, user_identifier=user_identifier)
-
-    def post(self):
-        print(session['id'])
-        return session['id']
+        game_uuid = uuid.uuid4()
+        presentation = ''
+        for letter in word:
+            presentation += '_'
+        Game.create_game(game_uuid, word, len(word))
+        return jsonify(word_length=len(word), presentation=presentation, game_uuid=game_uuid)
 
 
 class Letter(Resource):
@@ -54,34 +47,28 @@ class Letter(Resource):
             'user_identifier',
             required=True,
             help='No user_identifier provided',
-            location=['form','json']
+            location=['form', 'json']
         )
         super().__init__()
 
     def post(self):
         args = self.reqparse.parse_args()
-        game_id = Game.get(Game.user_identifier==args['user_identifier']).id
-        word = Game.get(Game.user_identifier==args['user_identifier']).word
+        game_id = Game.get(Game.user_identifier == args['user_identifier']).id
+        word = Game.get(Game.user_identifier == args['user_identifier']).word
         print(game_id)
         letters_used = ''
-        game_letters = LetterGuessed.select().where(LetterGuessed.game_id_id == game_id)
+        game_letters = LetterGuessed.select().where(LetterGuessed.game_id == game_id)
         for i in game_letters:
             letters_used += i.letter
         print(letters_used)
-        guessed_word = getGuessedWord(word,letters_used)
+        guessed_word = getGuessedWord(word, letters_used)
         print(guessed_word)
-        
-
-
-        
-
 
 game_api = Blueprint('resources.game', __name__)
 api = Api(game_api)
 api.add_resource(
     Word,
-    '/api/v1/word',
-    endpoint=''
+    '/api/v1/word'
 )
 
 api.add_resource(
