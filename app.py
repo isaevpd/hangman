8f099d3f1e1f86bfb76fa2921e2dc60c6fd3fe54
isaevpd <isaevpd4@gmail.com>
2017-11-12 17:03:06 +0200

    Add templates and index, game_over, hangman routes

--- app.py
+++ app.py
@@ -5,9 +5,17 @@ from resources.game import game_api
 app = Flask(__name__)
 app.register_blueprint(game_api)
     
-@app.route('/', methods=['GET'])
+@app.route('/hangman')
+def hangman():
+    return render_template('hangman.html')
+
+@app.route('/')
 def index():
-    return 'Home page should be here'
+    return render_template('index.html')
+
+@app.route('/game_over')
+def game_over():
+    return render_template('game_over.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
