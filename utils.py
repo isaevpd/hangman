83aa426087f498dde04b769aba6a02bd247ddc35
Pavel Isaev <pavel.isaev@yousician.com>
2018-04-01 00:20:51 +0200

    Remove load_words function

--- utils.py
+++ utils.py
@@ -3,9 +3,6 @@ import random
 WORDLIST_FILENAME = 'words.txt'
 
 
-def load_words():
-    return open(WORDLIST_FILENAME).readline().split()
-
-
-def choose_word(word_list):
-    return random.choice(word_list)
+def choose_word():
+    with open(WORDLIST_FILENAME) as word_list:
+        return random.choice(word_list.readline().split())
