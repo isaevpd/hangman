f8ce66b8ba0edfa9223788b02a5f5a7dc8370f91
Pavel Isaev <pavel.isaev@yousician.com>
2018-04-01 00:21:02 +0200

    Add more tests

--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
+import json
 import unittest
 
+from mongoengine import connection
+
 from app import app
-import json
+from models import Game
 
 
 class HangmanAPITest(unittest.TestCase):
@@ -11,11 +14,12 @@ class HangmanAPITest(unittest.TestCase):
         and run migrations on the test db
         """
         self.test_client = app.test_client()
-        db.connect()
-        db.create_tables([Game, LetterGuessed])
+        connection.disconnect()
+        connection.connect('hangman_test')
 
     def tearDown(self):
-        db.drop_tables([Game, LetterGuessed])
+        connection.get_connection().drop_database('hangman_test')
+        connection.get_connection().close()
 
     def test_post_word(self):
         """
@@ -29,6 +33,7 @@ class HangmanAPITest(unittest.TestCase):
             available_letters: all lowercase ascii letters
         }
         """
+        game_count = Game.objects.count()
         response = self.test_client.post('/api/v1/word')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
@@ -43,13 +48,46 @@ class HangmanAPITest(unittest.TestCase):
             set(data),
             expected_fields
         )
+        self.assertEqual(Game.objects.count(), game_count + 1)
 
+    def test_get_letter(self):
+        # create a game and save cookie
+        self.test_client.post('/api/v1/word')
+        response = self.test_client.get('/api/v1/letter')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
 
-if __name__ == '__main__':
-    import os
-    if os.environ.get('TESTING'):
-        unittest.main()
-    else:
-        raise RuntimeError(
-            'Please run "export TESTING=1" before running tests'
+        expected_fields = {
+            'word_length',
+            'representation',
+            'attempts_left',
+            'available_letters'
+        }
+        self.assertEqual(
+            set(data),
+            expected_fields
         )
+
+    def test_post_first_letter(self):
+        # create a game and save cookie
+        self.test_client.post('/api/v1/word')
+        response = self.test_client.post(
+            '/api/v1/letter',
+            data=json.dumps({'letter': 'a'}),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        game = Game.objects.first()
+
+        expected_data = {
+            'word_length': game.word_length,
+            'letter': 'a',
+            'representation': game.representation,
+            'available_letters': game.get_available_letters(),
+            'attempts_left': game.last_letter.attempts_left,
+            'status': game.status,
+            'message': game.last_letter.message,
+            'original_word': None
+        }
+        self.assertDictEqual(data, expected_data)
